ffd14825f89f5a10150bc97410e2395f3e51c562
Asdrubalini <asdrubalini@mail.com>
2023-11-24 23:04:15 +0000

    fix

--- Makefile
+++ Makefile
@@ -2,7 +2,10 @@ prepare:
 	python utils/preprocess.py --data_path data/stackoverflow --output_dir input/stackoverflow
 
 run:
-	python run_NQTM.py --data_dir input/stackoverflow --output_dir output --epoch=200
+	python run_NQTM.py --data_dir input/stackoverflow --output_dir output --epoch=2000
 
 evaluate:
 	python utils/TU.py --data_path output/top_words_T15_K50_1th
+
+develop:
+	NIXPKGS_ALLOW_UNFREE=1 nix develop --impure
--- flake.nix
+++ flake.nix
@@ -3,13 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
+    nixpkgs-nvidia.url = "https://github.com/nixos/nixpkgs/archive/0fdc7224a24203d9489bc52892e3d6121cacb110.tar.gz";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, nixpkgs-nvidia, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        pkgs-nvidia = import nixpkgs-nvidia {
           inherit system;
         };
       in
@@ -22,6 +27,13 @@
               scikitlearn
             ]))
           ];
+
+          shellHook = ''
+            export CUDA_PATH=${pkgs-nvidia.cudatoolkit}
+            export LD_LIBRARY_PATH=${pkgs-nvidia.linuxPackages.nvidia_x11}/lib:${pkgs-nvidia.ncurses5}/lib
+            export EXTRA_LDFLAGS="-L/lib -L${pkgs-nvidia.linuxPackages.nvidia_x11}/lib"
+            export EXTRA_CCFLAGS="-I/usr/include"
+          '';
         };
       }
     );
